a096f94adb5e81c7dc9acde6b313439cd7e4d5d3
colin-c <colin.cchn@gmail.com>
2022-09-25 14:18:19 -0700

    final commit

---
+++ answerQuestions.py
@@ -0,0 +1,32 @@
+from createQuestions import ProblemBank
+from createQuestions import AnswerBank
+
+
+# Define variable for values from different keys
+bank = AnswerBank.get("What is 2 + 2?")
+
+answer = input("""Select an option:
+                Press A for option A
+                Press B for option B
+                Press C for option C
+                Press D for option D""").upper()
+
+current = False
+
+if answer == AnswerBank.get("What is 2 + 2?"):
+    current = True
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
--- createQuestions.py
+++ createQuestions.py
@@ -4,47 +4,74 @@ from re import X
 
 # A dictionary of problems and their potential answers
 ProblemBank = {
-    "What is 2 + 2?": ["4", "7", "0", "9"]
+    "What is 2 + 2?": ["4", "7", "0", "9"],
+    "What is 5 + 3?": ["4", "8", "5", "9"]
 }
 
-
-# A dictionary of problems and their correct answer only
-ProblemMatch = {
-    "What is 2 + 2?": "4"
+# A dictionary of problems and their true answer
+AnswerBank = {
+    "What is 2 + 2?": "A",
+    "What is 5 + 3?": "B"
 }
+
 
 
 # Add the question to the Problem Bank
-def addProblem(question, correctAnswer, randomB, randomC, randomD):
-    ProblemBank[question] = [correctAnswer, randomB, randomC, randomD]
-    ProblemMatch[question] = correctAnswer
+def addProblem(question, answerA, answerB, answerC, answerD):
+    ProblemBank[question] = [answerA, answerB, answerC, answerD]
     print("Your question has been successfully added!")
+
+# Add the question to the AnswerBank with correct multiple choice option
+def addAnswer(question, answer):
+    AnswerBank[question] = answer
+    print("Correct answer and letter pairing has been confirmed")
 
 proceed = True
 
+
+def addQ(proceed):
 # Input a button to produce a certain output
-while proceed:
-    answer = input("""Add a new question?
-    Press A to added new question
-    Press D to delete an existing question
-    Press X to if not more questions to add""").upper()
+    while proceed:
+        answer = input("""Add a new question?
+        Press A to added new question
+        Press D to delete an existing question
+        Press V to view current existing questions
+        Press X to if not more questions to add""").upper()
 
-    if answer == "A":
-        (addProblem(input("What is the Question?: "), 
-                    input("What is the CORRECT Answer?: "), 
-                    input("Include WRONG Answer 1: "),
-                    input("Include WRONG Answer 2: "),
-                    input("Include WRONG Answer 3: ")))
-    
-    elif answer == "D":
-        key = input("Type in question you want to delete")
-        
-        if key in ProblemBank and ProblemMatch:
-            del ProblemBank[key]
-            del ProblemMatch[key] 
-        
+    # To add a question to ProblemBank and Problem Dictionary
+        if answer == "A":
+            question = input("Question: ")
+            (addProblem((question), 
+                        input("Answer (A): "), 
+                        input("Answer (B): "),
+                        input("Answer (C): "),
+                        input("Answer (D): ")))
+        # To add a character that matches with the CORRECT value
+            (addAnswer((question),
+                        input("Choose correct letter as true answer")))
 
-    elif answer == "X":
-        proceed = False
+            proceed = False
 
-    else: ("Oops! Something went wrong... try again.")
+    # To delete an existing question to ProblemBank and Problem Dictionary   
+        elif answer == "D":
+            key = input("Type in question you want to delete")
+            
+            if key in ProblemBank:
+                del ProblemBank[key] 
+                ("The question has been successful deleted")
+
+    # To view current questions, correct answers, and wrong answers from the ProblemBank
+        elif answer == "V":
+            print(ProblemBank)
+
+    # To end the session and return to previous screen
+        elif answer == "X":
+            proceed = False
+
+    # Inputing any other key will result in not but an error message
+        else: ("Oops! Something went wrong... try again.")
+
+if __name__ == "__main__":
+    addQ(proceed)
+
+
